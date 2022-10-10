8e88ea0bbc05259d3a038ef323b6350573fb5fca
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-10 03:37:42 +0100

    Add files via upload

---
+++ userAuth/php/register.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_POST['submit'])){
+    $username = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+registerUser($username, $email, $password);
+
+}
+
+function registerUser($username, $email, $password){
+    //save data into the file
+	$file = "../storage/users.csv";
+	$filename = fopen($file, "a");
+	$form = array($username, $email, $password);
+	if(fputcsv($filename, $form)!==false){
+		echo "User Successfully registered<br/>";
+?>
+<p align="center">Please <a href="../html/login.html">click here</a> to proceed to the login page</p>
+<?php
+	}else{
+		echo "User Registration failed";
+	}
+    fclose($filename);
+}
+
+?>
+
