37eb3f1b6a29b8a1d29435080a8ddeeb43bef926
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-16 21:20:40 +0100

    Update register.php

--- userAuth/php/register.php
+++ userAuth/php/register.php
@@ -14,7 +14,7 @@ function registerUser($username, $email, $password){
 	$filename = fopen($file, "a");
 	$form = array($username, $email, $password);
 	if(fputcsv($filename, $form)!==false){
-		echo "User Successfully registered<br/>";
+		echo "<p align='center'>User Successfully Registered</p>";
 ?>
 <p align="center">Please <a href="../html/login.html">click here</a> to proceed to the login page</p>
 <?php
