398404fe5aafda829a9b7dee3951f4a264ee557e
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-16 22:40:52 +0100

    Update reset.php

--- userAuth/php/reset.php
+++ userAuth/php/reset.php
@@ -7,7 +7,8 @@ if(isset($_POST['submit'])){
 }
 
 function resetPassword($email, $password){
-	
+    //open file and check if the username exist inside
+    //if it does, replace the password
 	$file = "../storage/users.csv";
 	$temp = "../storage/temp.csv";
 	$filename = fopen($file,"r");
@@ -15,23 +16,26 @@ function resetPassword($email, $password){
 	while(($filenames = fgetcsv($filename))!==false){
 		$name = $filenames[1];
 		if($email==$name){
-			$filenames[2] = $password;
-			echo "Password successfully changed";
-			fputcsv($write,$filenames);
-			file_put_contents('../storage/users.csv', file_get_contents('../storage/temp.csv')); 
-			unlink('../storage/temp.csv');
-			break;
+				$filenames[2] = $password;
+				echo "<p align='center' style='color:green'>Password successfully changed</p>";
+				fputcsv($write,$filenames);
+				file_put_contents('../storage/users.csv', file_get_contents('../storage/temp.csv')); 
+				unlink('../storage/temp.csv');
+				break;
 			?>
 				<p align="center">Please <a href="../html/login.html">click here</a> to proceed to the login page</p>
 			<?php
 		}else{
-			echo "<p align='center'>Email not present in our system</p>";
-			unlink('../storage/temp.csv');
+				echo "<p align='center' style='color:red'>Email not present in our system</p>";
+			?>
+				<p align="center">Please <a href="../html/resetpassword.html">click here</a> to proceed to the reset password page</p>
+			<?php
+				unlink('../storage/temp.csv');
 		}
 		
 	}
 	fclose($filename);
-	fclose($write);	
+	fclose($write);
+	
+	
 }
-
-
