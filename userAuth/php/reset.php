02a5e5d7cc5802f156814076d0bfdede1cc76416
Oyekunle Dare <115362169+OyekunleDare@users.noreply.github.com>
2022-10-16 21:59:36 +0100

    Update reset.php

--- userAuth/php/reset.php
+++ userAuth/php/reset.php
@@ -7,8 +7,7 @@ if(isset($_POST['submit'])){
 }
 
 function resetPassword($email, $password){
-    //open file and check if the username exist inside
-    //if it does, replace the password
+	
 	$file = "../storage/users.csv";
 	$temp = "../storage/temp.csv";
 	$filename = fopen($file,"r");
@@ -18,14 +17,21 @@ function resetPassword($email, $password){
 		if($email==$name){
 			$filenames[2] = $password;
 			echo "Password successfully changed";
+			fputcsv($write,$filenames);
+			file_put_contents('../storage/users.csv', file_get_contents('../storage/temp.csv')); 
+			unlink('../storage/temp.csv');
+			break;
+			?>
+				<p align="center">Please <a href="../html/login.html">click here</a> to proceed to the login page</p>
+			<?php
+		}else{
+			echo "<p align='center'>Email not present in our system</p>";
+			unlink('../storage/temp.csv');
 		}
-		fputcsv($write,$filenames);
+		
 	}
 	fclose($filename);
-	fclose($write);
-	
-	file_put_contents('../storage/users.csv', file_get_contents('../storage/temp.csv')); 
-	unlink('../storage/temp.csv');
+	fclose($write);	
 }
 
 
